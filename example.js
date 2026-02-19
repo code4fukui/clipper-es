fa8e1ddd650cfbb5a1142886bb94ec74e621a320
Taisuke Fukuno <fukuno@jig.jp>
2026-02-20 08:05:57 +0900

    add a simple example

---
+++ example.js
@@ -0,0 +1,12 @@
+import ClipperLib from "./clipper.js";
+
+console.log("version", ClipperLib.version);
+
+const lines = [
+  [ { X: 1, Y: 2 }, { X: 2, Y: 3 } ],
+  [ { X: 1, Y: 2 }, { X: 2, Y: 3 } ],
+];
+
+const scale = 100;
+ClipperLib.JS.ScaleUpPaths(lines, scale);
+console.log(lines);
