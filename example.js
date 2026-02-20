b9e6dff465293451a066e3774e2f323f40bb6199
Taisuke Fukuno <fukuno@jig.jp>
2026-02-20 16:04:44 +0900

    fix format

--- example.js
+++ example.js
@@ -3,8 +3,8 @@ import ClipperLib from "./clipper.js";
 console.log("version", ClipperLib.version);
 
 const lines = [
-  [ { X: 1, Y: 2 }, { X: 2, Y: 3 } ],
-  [ { X: 1, Y: 2 }, { X: 2, Y: 3 } ],
+  [{ X: 1, Y: 2 }, { X: 2, Y: 3 }],
+  [{ X: 1, Y: 2 }, { X: 2, Y: 3 }],
 ];
 
 const scale = 100;
